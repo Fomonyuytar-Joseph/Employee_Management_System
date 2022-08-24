aa13577088da73e7285838dce4b85a70d30e9ebf
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-24 15:47:54 +0100

    add sucess and failure component

---
+++ components/Form.js
@@ -0,0 +1,19 @@
+import React  from "react";
+import AddUserForm from "./AddUserForm";
+import UpdateUserForm from "./UpdateUserForm";
+
+
+
+
+
+function Form() {
+
+    return(
+        <AddUserForm/>
+
+    )
+
+   
+}
+
+export default Form;
